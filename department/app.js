2ec778f29c085a5afac3f0f486d23fb4fe8933ae
An1rey <andrey.ignatuk567@gmail.com>
2023-02-07 11:54:43 +0200

    Creation of entities

---
+++ department/app.js
@@ -0,0 +1,53 @@
+
+function Person (pName, pAge) {
+    this.name = pName;
+    this.age = pAge;
+    this.personInfo = function () {
+        console.group (`${this.name} info:`);
+        console.log ("Your name is: " + this.name);
+        console.log ("Your age is: " + this.age);
+        console.groupEnd ();
+    };
+};
+
+function Car (pModel,pYear) {
+    this.model = pModel;
+    this.year = pYear;
+    this.getCarInfo = function () {
+        console.group (`${this.model} info:`);
+        console.log ("Model of your car: " + this.model);
+        console.log ("Year of issue: " + this.year);
+        console.groupEnd();
+    };
+    this.ownerCar = function (person) {
+        console.log(person.name + " is the owner of the " + this.model + " " + this.year);
+    };
+};
+
+const owner = new Person (
+    pName = prompt('what is your name?'),
+    pAge = prompt('how old are you?')
+);
+const myCar = new Car(
+    pModel = prompt('What car do you own?'),
+    pYear = prompt('what year is your car?')
+);
+const owner1 = new Person (
+    pName = prompt('what is your name?'),
+    pAge = prompt('how old are you?')
+);
+const myCar1 = new Car(
+    pModel = prompt('What car do you own?'),
+    pYear = prompt('what year is your car?')
+);
+
+
+
+
+
+owner1.personInfo()
+myCar1.getCarInfo()
+myCar1.ownerCar(owner1)
+owner.personInfo();
+myCar.getCarInfo();
+myCar.ownerCar(owner);
